d565840043cd8e4783481eb82a38e14ba857eee6
suphakrit <suphakrit_kongkham@cmu.ac.th>
2023-03-03 19:14:03 +0700

    yehee

--- average.cpp
+++ average.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
 
+int main(int argc, char *argv[]){
+
+    float sum;
+     if(argc > 1)
+     {
+        for(int i = 0; i < argc ; i++ ){
+            sum += atof(argv[i]);
+        }
+        sum = sum/(argc-1);
+
+        cout <<"---------------------------------\n";
+        cout <<"Average of "<<argc-1<<" numbers = "<<sum <<"\n" ;
+        cout <<"---------------------------------\n";
+
+     }
+     else{
+        cout << "Please input numbers to find average.";
+     }
+     return 0;
+
+}
